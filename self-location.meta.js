e6a3a63609e6ea1d41f7817fc96d06e76c0ca539
Nux <eccenux+git@gmail.com>
2023-03-17 21:06:57 +0100

    1.1.0: less noise when not following

--- self-location.meta.js
+++ self-location.meta.js
@@ -2,15 +2,11 @@
 // @id             iitc-plugin-self-location@eccenux
 // @name           IITC plugin: Self location
 // @category       Misc
-// @version        1.0.0
+// @version        1.1.0
 // @namespace      https://github.com/jonatkins/ingress-intel-total-conversion
-// @description    [1.0.0] Self location tracker. Your position on the map. Obviously works best on a mobile device.
-// @include        https://*.ingress.com/intel*
-// @include        http://*.ingress.com/intel*
+// @description    [1.1.0] Self location tracker. Your position on the map. Obviously works best on a mobile device.
 // @match          https://*.ingress.com/intel*
 // @match          http://*.ingress.com/intel*
-// @include        https://*.ingress.com/mission/*
-// @include        http://*.ingress.com/mission/*
 // @match          https://*.ingress.com/mission/*
 // @match          http://*.ingress.com/mission/*
 // @match          https://intel.ingress.com/*
--- self-location.user.js
+++ self-location.user.js
@@ -5,12 +5,8 @@
 // @version        1.1.0
 // @namespace      https://github.com/jonatkins/ingress-intel-total-conversion
 // @description    [1.1.0] Self location tracker. Your position on the map. Obviously works best on a mobile device.
-// @include        https://*.ingress.com/intel*
-// @include        http://*.ingress.com/intel*
 // @match          https://*.ingress.com/intel*
 // @match          http://*.ingress.com/intel*
-// @include        https://*.ingress.com/mission/*
-// @include        http://*.ingress.com/mission/*
 // @match          https://*.ingress.com/mission/*
 // @match          http://*.ingress.com/mission/*
 // @match          https://intel.ingress.com/*
